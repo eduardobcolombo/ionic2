3c911b2dd5f98bd09706f8cb193e92bb87a57999
Eduardo de Brito Colombo <eduardobcolombo@gmail.com>
2017-06-06 17:56:45 -0300

    taking picture

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,45 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, LoadingController } from 'ionic-angular';
+import { Camera } from 'ionic-native';
 
 @Component({
+
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public base64Image: string;
+  OCRAD: any;
+  constructor(
+  public navCtrl: NavController,
+  public loadingCtrl: LoadingController
 
-  constructor(public navCtrl: NavController) {
+  ) {
 
   }
+
+  takePicture(){
+    Camera.getPicture({
+        destinationType: Camera.DestinationType.DATA_URL,
+        targetWidth: 1000,
+        targetHeight: 1000
+    }).then((imageData) => {
+      // imageData is a base64 encoded string
+        this.base64Image = "data:image/jpeg;base64," + imageData;
+    }, (err) => {
+        console.log(err);
+    });
+  }
+  analyze() {
+    let loader = this.loadingCtrl.create({
+     content: 'Analizando a foto...'
+    });
+    loader.present();
+    (<any>window).OCRAD(document.getElementById('imageOCR'), text => {
+      loader.dismissAll();
+      alert(text);
+      console.log(text);
+    });
+}
 
 }
